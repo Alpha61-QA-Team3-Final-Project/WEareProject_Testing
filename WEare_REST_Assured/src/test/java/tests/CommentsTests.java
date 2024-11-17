a43ad384c354d1d66b5feea4cd1a1a8cb973eed8
danielivanovQA61 <daniel.ivanov.a61@learn.telerikacademy.com>
2024-11-17 12:09:42 +0200

    update

--- WEare_REST_Assured/src/test/java/tests/CommentsTests.java
+++ WEare_REST_Assured/src/test/java/tests/CommentsTests.java
@@ -34,6 +34,20 @@ public class CommentsTests extends BaseTestSetup {
         Assert.assertTrue(response.getBody().asString().contains("commentId"),"Expected response body to contains commentID");
     }
 
+    @Test
+    public void editCommentTest(){
+//        createAndRegisterUserReceiver();
+        createAndRegisterUser();
+        loginUser();
+        createPost();
+//        loginUserReceiver();
+        //createComment("This is a comment");
+        response = createComment();
+        System.out.println("Comment body is" + JSONRequests.COMMENT_BODY);
+        System.out.println("Response body is " + response.getBody().asString());
+        Assert.assertEquals(response.statusCode(), 200, "Expected status code to be 200");
+        Assert.assertTrue(response.getBody().asString().contains(Constants.COMMENT_DESCRIPTION),"Expected response body to contains Comment");
+    }
 
 }
 
